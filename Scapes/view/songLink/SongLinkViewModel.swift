cfceaaa09677ae41276397090b177d45c0203292
Max Baumbach <bearjaw.dev@gmail.com>
2019-03-05 17:28:35 +0100

    Implement SongLinkViewModelProtocol

--- Scapes/view/songLink/SongLinkViewModel.swift
+++ Scapes/view/songLink/SongLinkViewModel.swift
@@ -19,17 +19,59 @@ protocol SongLinkViewModelProtocol {
 }
 
 final class SongLinkViewModel: SongLinkViewModelProtocol {
-    var items: [SongLinkViewData] = []
+    
+    var data: [SongLinkViewData] {
+        return items
+    }
     private var playlist: Playlist?
     private var remainingSongs: [Song] = []
-    
-    func subscribe(onInitial: () -> Void, onChange: () -> Void) {
-        
-    }
+    private let repo = SongRepository()
+    private var token: RepoToken?
+    private var items: [SongLinkViewData] = []
     
     private lazy var service: SongLinkProvider = {
         return SongLinkProvider()
     }()
     
+    func subscribe(onInitial: @escaping () -> Void, onChange: @escaping (Indecies) -> Void, onEmpty: @escaping () -> Void) {
+        token = repo.subscribe(onInitial: { [unowned self] newValue in
+            self.items = self.convert(newValue)
+            if self.items.isEmpty { onEmpty() }
+        }, onChange: { [unowned self] newValue, indecies  in
+            self.items = self.convert(newValue)
+            if self.items.isEmpty { onEmpty() }
+            onChange(indecies)
+        })
+    }
+    
+    func convert(_ value: [SongLink]) -> [SongLinkViewData] {
+        return value.map({ SongLinkViewData(url: $0.url,
+                                                     success: true,
+                                                     title: $0.title,
+                                                     artist: $0.artist,
+                                                     album: $0.album,
+                                                     index: $0.index)})
+    }
+    
+    func updatePlaylist(_ playlist: Playlist) {
+        self.playlist = playlist
+    }
+    
+    func fetchRemainingSongsIfNeeded() {
+        guard let playlist = playlist else { return }
+        service.provideCachedSongs(for: playlist, content: { [weak self] cache, remainingSongs in
+            guard let self = self else { return }
+            self.items.append(contentsOf: cache)
+            self.remainingSongs = remainingSongs
+            self.downloadLinksIfNeeded(songs: remainingSongs)
+        })
+    }
+    
+    private func downloadLinksIfNeeded(songs: [Song]) {
+        guard songs.isNonEmpty else { return }
+        service.search(in: songs) { _ in
+            // TODO: remove collection
+        }
+    }
     
 }
