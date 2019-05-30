be14ccc3664d55ebc2a3e2e6441e54d6468fd5f7
Max Baumbach <bearjaw.dev@gmail.com>
2019-05-30 12:32:13 +0200

    Clean up extension

--- Scapes/libs/Avenue/Bundle-Extensions.swift
+++ Scapes/libs/Avenue/Bundle-Extensions.swift
@@ -9,23 +9,23 @@
 import Foundation
 
 public extension Bundle {
-	public static var appName: String {
-		guard let str = main.object(forInfoDictionaryKey: "CFBundleName") as? String else { return "" }
-		return str
-	}
-
-	public static var appVersion: String {
-		guard let str = main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String else { return "" }
-		return str
-	}
-
-	public static var appBuild: String {
-		guard let str = main.object(forInfoDictionaryKey: "CFBundleVersion") as? String else { return "" }
-		return str
-	}
-
-	public static var identifier: String {
-		guard let str = main.object(forInfoDictionaryKey: "CFBundleIdentifier") as? String else { return "" }
-		return str
-	}
+    static var appName: String {
+        guard let str = main.object(forInfoDictionaryKey: "CFBundleName") as? String else { return "" }
+        return str
+    }
+    
+    static var appVersion: String {
+        guard let str = main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String else { return "" }
+        return str
+    }
+    
+    static var appBuild: String {
+        guard let str = main.object(forInfoDictionaryKey: "CFBundleVersion") as? String else { return "" }
+        return str
+    }
+    
+    static var identifier: String {
+        guard let str = main.object(forInfoDictionaryKey: "CFBundleIdentifier") as? String else { return "" }
+        return str
+    }
 }
