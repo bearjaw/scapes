09a333b5b29b71bd206de32cc0dd5c8831f3f813
Max Baumbach <bearjaw.dev@gmail.com>
2019-07-27 19:14:19 +0200

    Create ErrorKit
    - Add ErrorKit as local Swift package

---
+++ ErrorKit/Tests/ErrorKitTests/ErrorKitTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+@testable import ErrorKit
+
+final class ErrorKitTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
