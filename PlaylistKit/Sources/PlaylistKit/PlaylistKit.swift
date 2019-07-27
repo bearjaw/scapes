4a14bf8f92b83dee8ccb3c3480ca514cabb1fa19
Max Baumbach <bearjaw.dev@gmail.com>
2019-07-27 20:40:06 +0200

    Fetch song

--- PlaylistKit/Sources/PlaylistKit/PlaylistKit.swift
+++ PlaylistKit/Sources/PlaylistKit/PlaylistKit.swift
@@ -1,17 +1,27 @@
-
 import MediaPlayer
 
 final public class PlaylistKit {
     
-    static func fetchPlaylist(source: MusicSource, playlists: @escaping (Result<[CorePlaylist], Error>, songs: () -> Void) -> Void) {
+    static func fetchPlaylist(source: MusicSource, playlists: @escaping (Result<[CorePlaylist], Error>) -> Void) {
         PlaylistAccess.fetchPlaylists(source: source) { result in
             switch result {
             case .success(let items):
                 let parser = PlaylistParser()
-                let parser.parse(collections: items, includingSongs: false)
+                let parsed = parser.parse(collections: items)
+                playlists(.success(parsed))
             case .failure(let error):
-                dump(error)
+                playlists(.failure(error))
             }
         }
+    }
+    
+    static func fetchSongs(forPlaylist identifier: UInt64, songs: (Result<[CorePlaylistItem], MusicKitError>) -> Void) {
+        guard let playlist = PlaylistAccess.fetchPlaylist(forIdentifier: identifier) else {
+            songs(.failure(.notFound(identifier)))
+            return
+        }
+        let parser = PlaylistParser()
+        let result = parser.parseSongs(forPlaylist: playlist)
+        songs(.success(result))
     }
 }
