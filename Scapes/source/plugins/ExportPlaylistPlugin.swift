5427d9d7608a6d68a190d24fb7ebca7419ce8d69
Max Baumbach <bearjaw.dev@gmail.com>
2019-08-18 16:36:57 +0200

    Add Export playlist plugin

---
+++ Scapes/source/plugins/ExportPlaylistPlugin.swift
@@ -0,0 +1,13 @@
+//
+//  ExportPlaylistPlugin.swift
+//  Scapes
+//
+//  Created by Max Baumbach on 18/08/2019.
+//  Copyright © 2019 Scapes. All rights reserved.
+//
+
+import UIKit
+
+final class ExportPlaylistPlugin: NSObject {
+
+}
