07b6f99aa73a063a76a6e970b5f35566221b5721
Max Baumbach <bearjaw.dev@gmail.com>
2019-08-18 13:53:33 +0200

    Add reusable plugin

---
+++ Scapes/source/plugins/FetchPlaylistsPlugin.swift
@@ -0,0 +1,49 @@
+//
+//  FetchSongsPlugin.swift
+//  Scapes
+//
+//  Created by Max Baumbach on 18/08/2019.
+//  Copyright © 2019 Scapes. All rights reserved.
+//
+
+import PlaylistKit
+import Foundation
+import os
+
+protocol Plugin {
+    var type: Int { get }
+}
+
+final class FetchPlaylistsPlugin {
+    
+    func fetchPlaylists(onCompletion: @escaping ([Playlist]) -> Void) {
+        PlaylistKit.fetchPlaylist(source: .local) { result in
+            switch result {
+            case let .success(items):
+                let playlists: [Playlist] = items.map { item in
+                    let artwork = PlaylistKit.fetchArtwork(forType: .playlist(identifier: item.localPlaylistIdentifier))
+                    let playlist = Playlist(name: item.name, count: item.itemCount, identifier: item.localPlaylistIdentifier, artwork: artwork)
+                    return playlist
+                }
+                DispatchQueue.main.async {
+                    onCompletion(playlists)
+                }
+            case let .failure(error):
+                onCompletion([])
+                os_log("%@", error.localizedDescription)
+            }
+        }
+    }
+    
+}
+
+extension FetchPlaylistsPlugin: Plugin {
+    var type: Int {
+        return ScapesPuginType.fetchPlaylist.rawValue
+    }
+}
+
+enum ScapesPuginType: Int {
+    case fetchPlaylist = 0
+    case fetchSongs
+}
