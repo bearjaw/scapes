b13bc908e2a1a982f77b0383a5409be172f258db
Max Baumbach <bearjaw.dev@gmail.com>
2019-08-18 16:51:19 +0200

    Implement export playlist feature

--- Scapes/source/plugins/ExportPlaylistPlugin.swift
+++ Scapes/source/plugins/ExportPlaylistPlugin.swift
@@ -9,5 +9,28 @@
 import UIKit
 
 final class ExportPlaylistPlugin: NSObject {
+    
+    func exportPlaylist(_ playlist: Playlist, songs: [SongLinkIntermediate], onCompletion: @escaping (Bool) -> Void) {
+        var exportString = playlist.name
+        DispatchQueue.global(qos: .userInitiated).async {
+            for item in songs {
+                if item.url.isEmpty {
+                    exportString.append(contentsOf: "\(item.title) \(item.artist) \n Couldn't find that song.")
+                } else {
+                    exportString.append(contentsOf: "\(item.title) - \(item.artist) \n URL: \(item.url) \n\n")
+                }
+            }
+            DispatchQueue.main.async {
+                let pasteBoard = UIPasteboard.general
+                pasteBoard.string = exportString
+                let generator = UINotificationFeedbackGenerator()
+                generator.notificationOccurred(.success)
+                onCompletion(true)
+            }
+        }
+    }
+}
 
+extension ExportPlaylistPlugin: Plugin {
+    var type: Int { ScapesPluginType.export.rawValue }
 }
