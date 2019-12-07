641e686517c1c10a8ddf3e0d0f7f8dd88f089c06
Max Baumbach <bearjaw.dev@gmail.com>
2019-12-07 20:01:46 +0100

    Add viewModel init to viewcontroller

--- Scapes/source/view/musicAnalysis/MusicAnalysisViewController.swift
+++ Scapes/source/view/musicAnalysis/MusicAnalysisViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 final class MusicAnalysisViewController: UIViewController {
-
+    
+    private var viewModel: MusicAnalysisViewModelProtocol
+    
+    // MARK: - Lifecycle begin
+    
+    init(viewModel: MusicAnalysisViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+        title = "Analysis"
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureNavbar()
         // Do any additional setup after loading the view.
     }
     
